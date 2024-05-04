9757d5e04f7b2f4bec022f745f3c9c20c8a34400
Swapvp <swapneshpawar1994@gmail.com>
2024-05-04 11:28:25 +0530

    star rating

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import StarRating from "./Components/StarRating";
+
+function App() {
+  return (
+    <>
+      <StarRating noOfStars={10}></StarRating>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/StarRating.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { IoIosStar } from "react-icons/io";
+import "./Styles.css";
+
+export default function StarRating({ noOfStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+
+        return (
+          <IoIosStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+}
